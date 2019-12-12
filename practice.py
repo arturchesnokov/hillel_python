d4a45221d60c12b89be1bd53067d6d9b7bfada1f
Artur Chesnokov <artur.chesnokov@gmail.com>
2019-12-12 19:33:58 +0200

    init commit

---
+++ practice.py
@@ -0,0 +1,15 @@
+class Human:
+    c = 4
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+h1 = Human('aaa', 'bbb')
+
+print(h1.a)
+print(h1.c)
+
+print(Human.c)
+print(h1.__dict__)
